b7f0b70ee20f337743d3ddde4a7a10e572855dce
Jacek Podkanski <jacek.podkanski@conceptlifesciences.com>
2018-07-18 12:32:58 +0100

    removed byebug

--- lib/roo/xls/excel.rb
+++ lib/roo/xls/excel.rb
@@ -24,7 +24,6 @@ module Roo
       file_warning = options[:file_warning] || :error
       mode = options[:mode] || 'rb+'
 
-
       if is_stream?(filename)
         @workbook = ::Spreadsheet.open(filename, mode)
       else
@@ -44,25 +43,12 @@ module Roo
         end
       end
 
-      # spannings from the corresponding ods file
-      # => {[1, 3]=>{:columns=>"16", :rows=>"2"}, [1, 19]=>{:columns=>"1", :rows=>"4"}, [3, 3]=>{:columns=>"16", :rows=>"1"}, [5, 1]=>{:columns=>"6", :rows=>"1"}, [5, 7]=>{:columns=>"1", :rows=>"9"}, [5, 8]=>{:columns=>"11", :rows=>"1"}, [6, 1]=>{:columns=>"6", :rows=>"2"}, [6, 8]=>{:columns=>"1", :rows=>"8"}, [6, 9]=>{:columns=>"1", :rows=>"8"}, [6, 10]=>{:columns=>"1", :rows=>"8"}, [6, 11]=>{:columns=>"1", :rows=>"8"}, [6, 12]=>{:columns=>"1", :rows=>"8"}, [6, 13]=>{:columns=>"1", :rows=>"8"}, [6, 14]=>{:columns=>"1", :rows=>"8"}, [6, 15]=>{:columns=>"1", :rows=>"8"}, [6, 16]=>{:columns=>"1", :rows=>"8"}, [6, 17]=>{:columns=>"1", :rows=>"8"}, [6, 18]=>{:columns=>"1", :rows=>"8"}, [8, 1]=>{:columns=>"2", :rows=>"1"}, [8, 5]=>{:columns=>"2", :rows=>"1"}, [9, 1]=>{:columns=>"2", :rows=>"1"}, [9, 5]=>{:columns=>"2", :rows=>"1"}, [10, 1]=>{:columns=>"3", :rows=>"1"}, [10, 5]=>{:columns=>"2", :rows=>"1"}, [11, 1]=>{:columns=>"3", :rows=>"1"}, [11, 5]=>{:columns=>"2", :rows=>"1"}, [11, 19]=>{:columns=>"1", :rows=>"2"}, [12, 1]=>{:columns=>"5", :rows=>"1"}, [12, 6]=>{:columns=>"1", :rows=>"2"}, [13, 1]=>{:columns=>"2", :rows=>"1"}, [14, 1]=>{:columns=>"2", :rows=>"1"}, [14, 19]=>{:columns=>"1", :rows=>"2"}, [15, 1]=>{:columns=>"2", :rows=>"1"}, [16, 1]=>{:columns=>"2", :rows=>"1"}, [17, 1]=>{:columns=>"2", :rows=>"1"}, [18, 1]=>{:columns=>"2", :rows=>"1"}, [19, 1]=>{:columns=>"2", :rows=>"1"}, [20, 1]=>{:columns=>"2", :rows=>"1"}, [21, 1]=>{:columns=>"2", :rows=>"1"}, [22, 1]=>{:columns=>"2", :rows=>"1"}, [23, 1]=>{:columns=>"2", :rows=>"1"}, [24, 1]=>{:columns=>"2", :rows=>"1"}, [25, 1]=>{:columns=>"2", :rows=>"1"}, [26, 1]=>{:columns=>"2", :rows=>"1"}, [27, 1]=>{:columns=>"2", :rows=>"1"}, [28, 1]=>{:columns=>"2", :rows=>"1"}, [29, 1]=>{:columns=>"2", :rows=>"1"}, [30, 1]=>{:columns=>"2", :rows=>"1"}, [31, 1]=>{:columns=>"2", :rows=>"1"}, [32, 1]=>{:columns=>"2", :rows=>"1"}, [33, 1]=>{:columns=>"2", :rows=>"2"}, [33, 3]=>{:columns=>"1", :rows=>"2"}, [33, 4]=>{:columns=>"1", :rows=>"2"}, [33, 5]=>{:columns=>"3", :rows=>"2"}, [33, 8]=>{:columns=>"3", :rows=>"2"}, [33, 11]=>{:columns=>"2", :rows=>"2"}, [33, 13]=>{:columns=>"2", :rows=>"2"}, [33, 15]=>{:columns=>"4", :rows=>"2"}, [33, 19]=>{:columns=>"1", :rows=>"2"}}
-
-      byebug
-      13==13
-      # inspecting imported file
-      # @workbook.worksheets.first
-      # pp  @workbook.worksheets.first.rows[13]
-      # merged_cells seem to be pairs of ranges starting at 0
-
-      # aaa = OldExcel.new OldExcel.test_file
-      # aaa.workbook.worksheets.first.merged_cells
-
       super(filename, options)
       @formula = {}
       @fonts = {}
     end
 
-    def spannings()
+    def spannings
       sheet_index = sheets.find_index default_sheet
       data = worksheets[sheet_index].merged_cells
 
@@ -80,7 +66,6 @@ module Roo
         columns = spany + 1
         newhash = { [r, c] => { columns: columns, rows: rows } }
         result.merge! newhash
-        # byebug if xfrom <= 4
         result
       end
       result
@@ -102,8 +87,8 @@ module Roo
     # this method lets you find the worksheet with the most data
     def longest_sheet
       sheet(worksheets.inject do |m, o|
-        o.row_count > m.row_count ? o : m
-      end.name)
+              o.row_count > m.row_count ? o : m
+            end.name)
     end
 
     # returns the content of a cell. The upper left corner is (1,1) or ('A',1)
@@ -291,11 +276,11 @@ module Roo
           next if cell.nil? # skip empty cells
           next if cell.class == ::Spreadsheet::Formula && cell.value.nil? # skip empty formula cells
           value_type, v =
-            if date_or_time?(row, cell_index)
-              read_cell_date_or_time(row, cell_index)
-            else
-              read_cell(row, cell_index)
-            end
+                      if date_or_time?(row, cell_index)
+                        read_cell_date_or_time(row, cell_index)
+                      else
+                        read_cell(row, cell_index)
+                      end
           formula = tr = nil # TODO:???
           col_index = cell_index + 1
           font = row.format(cell_index).font
